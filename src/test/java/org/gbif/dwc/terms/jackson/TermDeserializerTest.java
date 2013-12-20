08c55bd24320acd31eb0961f574aa5ccb51120e5
Markus Döring <mdoering@gbif.org>
2013-12-20 16:55:42 +0100

    Adding new map key term test

--- src/test/java/org/gbif/dwc/terms/jackson/TermDeserializerTest.java
+++ src/test/java/org/gbif/dwc/terms/jackson/TermDeserializerTest.java
@@ -5,6 +5,8 @@ import org.gbif.dwc.terms.GbifTerm;
 import org.gbif.dwc.terms.Term;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
@@ -26,5 +28,22 @@ public class TermDeserializerTest {
       String json = mapper.writeValueAsString(t);
       assertEquals(t, mapper.readValue(json, Term.class));
     }
+  }
+
+  @Test
+  public void testTermMap() throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    Integer counter = 0;
+    Map<Term, Integer> terms = new HashMap<Term, Integer>();
+    for (Term t : DwcTerm.values()) {
+      terms.put(t, counter++);
+    }
+
+    for (Term t : GbifTerm.values()) {
+      terms.put(t, counter++);
+    }
+
+    String json = mapper.writeValueAsString(terms);
+    assertEquals(terms.size(), mapper.readValue(json, HashMap.class).size());
   }
 }
