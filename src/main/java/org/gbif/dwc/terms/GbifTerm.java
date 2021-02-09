3a2ffbe1744e45b0afe723af7464c83e380b875f
fmendezh <federicomh@gmail.com>
2021-02-09 15:44:10 -0600

    reverting adding a new GbifTerm.iucnRedListCategory, an IUCN RedList Category already exists

--- src/main/java/org/gbif/dwc/terms/GbifTerm.java
+++ src/main/java/org/gbif/dwc/terms/GbifTerm.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2021 Global Biodiversity Information Facility (GBIF)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.gbif.dwc.terms;
 
 import java.io.Serializable;
@@ -285,12 +270,7 @@ public enum GbifTerm implements Term, AlternativeNames, Serializable {
 
   // To support user identifiers, like ORCID and WIKIDATA and etc.
   recordedByID(DwcTerm.GROUP_OCCURRENCE),
-  identifiedByID(DwcTerm.GROUP_IDENTIFICATION),
-
-  //IUCN Red List category
-  iucnRedListCategory(DwcTerm.GROUP_TAXON);
-
-
+  identifiedByID(DwcTerm.GROUP_IDENTIFICATION);
 
   private static final String PREFIX = "gbif";
   private static final String NS = "http://rs.gbif.org/terms/1.0/";
