dada47e43624d6a2cb9712872d332cf78135907a
gbif-jenkins <dev@gbif.org>
2026-01-07 07:56:40 +0000

    Publishing Site Docs for dwc-api:2.1.8-SNAPSHOT

--- testapidocs/package-search-index.js
+++ testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.gbif.dwc.terms"},{"l":"org.gbif.dwc.terms.jackson"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.gbif.dwc.terms"},{"l":"org.gbif.dwc.terms.jackson"}];updateSearchResults();
